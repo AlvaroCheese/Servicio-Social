75760792855bc5f0f3fe6aec652c92d4187dd793
AlvaroCheese <165858251+AlvaroCheese@users.noreply.github.com>
2024-05-05 17:08:32 -0600

    Mas o menos

---
+++ Card ID/src/pages/Group.jsx
@@ -0,0 +1,65 @@
+import styles from "./Group.module.css";
+
+const Group = () => {
+  return (
+    <div className={styles.pexelsNicoleAvagliano270665Parent}>
+      <div className={styles.pexelsNicoleAvagliano270665} />
+      <div className={styles.groupChild} />
+      <img className={styles.groupItem} alt="" src="/rectangle-11@2x.png" />
+      <div className={styles.groupChild} />
+      <div className={styles.fotoDelUsuario}>[Foto del Usuario]</div>
+      <div className={styles.rectangleDiv} />
+      <header className={styles.rectangleHeader} />
+      <main className={styles.rectangleMain} />
+      <div className={styles.nombre}>Nombre</div>
+      <div className={styles.fechaDeIngreso}>Fecha de Ingreso</div>
+      <div className={styles.deAbrilDel}>12 de Abril del 2024</div>
+      <div className={styles.unitedFederationOf}>
+        United Federation of Science International
+      </div>
+      <div className={styles.jorgeMartnezLozano}>Jorge Martínez Lozano</div>
+      <section className={styles.rectangleSection} />
+      <div className={styles.descripcin}>Descripción</div>
+      <div className={styles.holaSoyJorge}>
+        Hola soy Jorge, estudié Ingeniería Bioquimica en la universidad de
+        palermo y estudié una maestría en España
+      </div>
+      <section className={styles.groupChild1} />
+      <div className={styles.rol}>Rol</div>
+      <div className={styles.gerenteDeExperiencias}>
+        Gerente de experiencias
+      </div>
+      <section className={styles.groupChild2} />
+      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+      <img
+        className={styles.pexelsThisisengineering39125Icon}
+        alt=""
+        src="/pexelsthisisengineering3912513-1@2x.png"
+      />
+      <img
+        className={styles.pexelsNicoleAvagliano270665Icon}
+        alt=""
+        src="/pexelsnicoleavagliano2706654.svg"
+      />
+      <main className={styles.groupChild3} />
+      <img className={styles.image7Icon} alt="" src="/image-7@2x.png" />
+      <button className={styles.rectangleButton} />
+      <button className={styles.copiarLink}>Copiar link</button>
+      <button className={styles.carrera}>Carrera</button>
+      <div className={styles.ingenieraMecnica}>Ingeniería Mecánica</div>
+      <div className={styles.proyecto}>Proyecto:</div>
+      <div className={styles.ecolife}>EcoLife</div>
+      <div className={styles.habilidades}>Habilidades:</div>
+      <div className={styles.diseoCadPythonContainer}>
+        <ul className={styles.diseoCadPythonImpresin3d}>
+          <li className={styles.diseoCad}>Diseño CAD</li>
+          <li className={styles.diseoCad}>Python</li>
+          <li className={styles.diseoCad}>Impresión 3d</li>
+          <li>CNC</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Group;
